70fcaef72a643386d7a062cc8eab41333f3557b1
diego <240680@utxicotepec.edu.mx>
2026-02-20 14:17:22 -0600

    se agrego el catalogo de productos

---
+++ my-react-app/src/Productos.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import api from "./Services/api";    
+import "./Productos.css";
+
+
+function Productos () {
+    const [productos, setProductos] = useState([]);
+    const [cargando, setCargando] = useState(true);
+    useEffect(() => {
+        const obtenerProductos = async () => {
+            try {
+                const response = await api.get('/products');
+                setProductos(response.data);
+            } catch (error) {
+                console.error('Error al obtener los productos:', error);
+            }finally {
+                setCargando(false);
+            }
+        };
+        obtenerProductos();
+    },[]);
+    
+    if (cargando) return <p>Cargando productos...</p>;
+       
+    return (
+        <div className = "productosDiv">
+            <h1>Productos disponibles</h1>
+            {productos.map((producto) => (
+                <div key={producto.id}>
+                    <p>{producto.title}</p>
+                    <p>{producto.price}</p>
+                    <img src={producto.image} />
+                </div>
+            ))}
+        </div> 
+    )
+}
+
+
+export default Productos
